63316b375db2e8c708a5702eeaa9885fa7beffd3
RUI <115133652+RuisanA@users.noreply.github.com>
2026-06-01 00:39:22 +0900

    Update rbot.js

--- app/rbot.js
+++ app/rbot.js
@@ -1610,7 +1610,7 @@ client.on("interactionCreate", async (interaction) => {
         role = interaction.options.getRole("ロール") || "undefined";
       const embed = new MessageEmbed()
         .setTitle("ぷにぷに代行")
-        .setDescription(`メールアドレスとパスワードに間違いがないようにしてください。ワイポ代行の際は強敵取得は無料です\n\n1.10万ワイポ 1000円\n2.20万ワイポ 2000円\n3.30万ワイポ 3000円\n4.40万ワイポ 3500円\n5.50万ワイポ 4000円\n6.イベ終わりまで 3000円\n7.強敵取得 300円\n8.強敵完凸 800円\n9.ステージ進行 500円\n10.ウォッチ作成代行 1000円\n\nお支払い方法 PayPay`) 
+        .setDescription(`メールアドレスとパスワードに間違いがないようにしてください。ワイポ代行の際は強敵取得は無料です\n\n1.強敵取得 300円\n2.強敵完凸 1000円\n3.秘伝書カンスト 2000円\n4.スキル書カンスト 3000円\n5.ステージ進行 1000円\n\nお支払い方法 PayPay`) 
         .setColor("RANDOM")
       interaction.reply({
         embeds: [embed],
